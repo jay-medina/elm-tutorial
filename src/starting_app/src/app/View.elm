8ad319309102f5b219c31c857f7c1526ce886946
Jose Medina <josemedina921@gmail.com>
2018-08-18 12:44:22 -0700

    Getting the players view working

--- src/starting_app/src/app/View.elm
+++ src/starting_app/src/app/View.elm
@@ -3,8 +3,14 @@ module View exposing (..)
 import Html exposing (Html, div, text)
 import Models exposing (Model)
 import Msgs exposing (Msg)
+import Players.List
+
+
+page : Model -> Html Msg
+page { players } =
+    Players.List.view players
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ text model ]
+    div [] [ page model ]
